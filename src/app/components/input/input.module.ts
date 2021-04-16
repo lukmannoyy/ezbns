c28139458e3b2cf76f0cba86a6fb04b171fcb48f
lukmannoyy <lukman.hkazmi@gmail.com>
2021-04-16 15:56:23 +0800

    first commit

---
+++ src/app/components/input/input.module.ts
@@ -0,0 +1,13 @@
+import { InputComponent } from './input.component';
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@NgModule({
+  declarations: [InputComponent],
+  imports: [CommonModule, IonicModule],
+  exports: [InputComponent],
+})
+export class InputModule {}
+
